a426300f70f5b36c10ac6557902a4fa7d3db246c
Rakesh <rakesh.k.gorai@gmail.com>
2023-11-14 20:06:23 +0530

    Keybindings configuration

---
+++ lua/user/keymaps.lua
@@ -0,0 +1,41 @@
+local opts = { noremap = true, silent = true }
+
+local term_opts = { silent = true }
+
+-- Shorten function name
+local keymap = vim.api.nvim_set_keymap
+
+--Remap space as leader key
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Navigate buffers
+keymap("n", "<TAB>", ":bnext<CR>", opts)											--Switch to next buffer file using Tab Key
+keymap("n", "<S-TAB>", ":bprevious<CR>", opts) 								--Switch to previous buffer(file) with Shift+Tab
+keymap("n", "<C-s>", ":w<CR>", opts) 													--save file with Ctrl+S
+keymap("n", "<leader>bd", ":bd<cr>", opts)										--close buffer with "Space + b + d"
+
+-- Visual --
+-- Stay in indent mode
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+-- Move text up and down
+keymap("v", "<A-j>", ":m .+1<CR>==", opts)
+keymap("v", "<A-k>", ":m .-2<CR>==", opts)
+keymap("v", "p", '"_dP', opts)
+
+-- Visual Block --
+-- Move text up and down
+keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
+keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+
+-- Terminal --
+-- Better terminal navigation
+keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
+keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
+keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
+keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
